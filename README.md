77d88ef719c8a24f6b7d3dddd77cdb5882dcf137
Junyoung Shin <jun021222@naver.com>
2023-10-05 16:20:00 +0900

    Add convert.py

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-# Arcacon-Downloader v0.0.1
+# Arcacon-Downloader v0.0.2
+
+## Release Note
+### v0.0.2
+Now supports converting images to `.png`, videos to `.gif`
 
 ## Dependencies
 * python >= 3.8
 * Download packages using pip
 ```sh
-pip install beautifulsoup4 requests
+pip install beautifulsoup4 requests opencv-python imageio
 ```
 
 ## Usage
 ```sh
 $ python main.py <url>
+$ python convert.py <id>
 ```
 
 ### Example
 ```sh
 $ python main.py https://arca.live/e/31415
+$ python convert.py 31415
 ```
---
+++ convert.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import imageio
+import os
+import sys
+
+path = sys.argv[1]
+
+img_path = os.path.join(path, 'img')
+vid_path = os.path.join(path, 'vid')
+png_path = os.path.join(path, 'png')
+gif_path = os.path.join(path, 'gif')
+
+os.makedirs(png_path, exist_ok=True)
+os.makedirs(gif_path, exist_ok=True)
+
+# for i in os.listdir(img_path):
+#     name = i.split('.')[0]
+#     img = cv.imread(os.path.join(img_path, i), cv.IMREAD_UNCHANGED)
+#     cv.imwrite(os.path.join(png_path, f'{name}.png'), img)
+
+for i in os.listdir(vid_path):
+    name = i.split('.')[0]
+    
+    cap = cv.VideoCapture(os.path.join(vid_path, i))
+    fps = cap.get(cv.CAP_PROP_FPS)
+
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame[:,:,::-1]) # BGR -> RGB
+    
+    imageio.mimsave(os.path.join(gif_path, f'{name}.gif'), frames, fps=fps, loop=0)
